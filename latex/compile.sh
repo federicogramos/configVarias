a6310e9ef4c7eabbc2eff16dfee29292b18ed6ae
Federico Ramos <federico.g.ramos@gmail.com>
2026-08-26 12:59:59 -0300

    clean out flag

--- latex/compile.sh
+++ latex/compile.sh
@@ -8,5 +8,7 @@
 ## -c : Indica que el siguiente argumento es el comando de código Python a ejecutar
 FLAGS="-B -u -c"
 
+CLEAN_OUTPUT=0  ## Archivos temporales de salida.
+
 ## Ejecución del rearmado.
-"/c/Users/feder/.local/bin/python3.14.exe" $FLAGS "import sys; sys.path.append('C:/fgr/16gb/lib/libPython'); import fgrLib; fgrLib.procesar_y_compilar_informe('tp_final.ipynb', 'tp_final.tex')"
+"/c/Users/feder/.local/bin/python3.14.exe" $FLAGS "import sys; sys.path.append('C:/fgr/16gb/lib/libPython'); import fgrLib; fgrLib.procesar_y_compilar_informe('lab_plan.ipynb', 'lab_plan.tex', $CLEAN_OUTPUT)"
